5e54069b05cc7301cd261f2436291e4ac893be27
ceoxevello <ak.kumshey@gmail.com>
2021-08-29 18:32:20 +0530

    one of last views

--- OneDrive/Desktop/xevello/front-end/src/app/app-routing.module.ts
+++ OneDrive/Desktop/xevello/front-end/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillingPosComponent } from './components/billing-pos/billing-pos.component';
+import { CreateWebsiteComponent } from './components/create-website/create-website.component';
+import { EcommerceComponent } from './components/ecommerce/ecommerce.component';
 import { IndexHomeComponent } from './components/index-home/index-home.component';
+import { LogoDesignComponent } from './components/logo-design/logo-design.component';
 
 const routes: Routes = [
   {path : '', component: IndexHomeComponent},
-  {path: 'home', component: IndexHomeComponent}
+  {path: 'home', component: IndexHomeComponent},
+  {path: 'create-website', component: CreateWebsiteComponent},
+  {path: 'online-store', component: EcommerceComponent},
+  {path: 'logo-design', component: LogoDesignComponent},
+  {path: 'pos-billing', component: BillingPosComponent}
+
+
 ];
 
 @NgModule({
@@ -13,4 +23,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [IndexHomeComponent]
+export const routingComponents = [IndexHomeComponent, CreateWebsiteComponent, EcommerceComponent, LogoDesignComponent, BillingPosComponent]
